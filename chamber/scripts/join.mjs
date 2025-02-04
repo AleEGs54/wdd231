29fc1f73562f49a5c97d2cd27ac92f8cb43de997
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-02-04 13:14:10 -0700

    fix: update join link and improve styling for join page inputs

--- chamber/scripts/join.mjs
+++ chamber/scripts/join.mjs
@@ -15,7 +15,7 @@ const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#animateme');
 
 hamButton.addEventListener('click', function () {
-    hamButton.classList.toggle('open'); //This is to switch from the hmburguer button to the X icon
+    hamButton.classList.toggle('open'); //This is to switch from the hamburguer button to the X icon
     navigation.classList.toggle('open'); //This is to display (as block) the menu options
 
 });
