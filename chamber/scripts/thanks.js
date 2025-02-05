13a4622077a9da0c507651296e8f01e3cb64f3b7
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-02-04 18:21:44 -0700

    fix: convert applicant's title to uppercase for improved visibility on thank you page

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -36,7 +36,7 @@ container.innerHTML = `
 <p>E-mail <strong>${showInfo("email")}</strong></p>
 <h2>Business Info</h2>
 <p>Business/Organization's Name: <strong>${showInfo("Bname")}</strong></p>
-<p>Applicant's title in the business: <strong>${showInfo("title")}</strong></p>
+<p>Applicant's title in the business: <strong>${showInfo("title").toUpperCase()}</strong></p>
 <p>Type of Membership: <strong>${showInfo("membership-level")}</strong></p>
 <p>Business description: <strong>${showInfo("description")}</strong></p>
 
