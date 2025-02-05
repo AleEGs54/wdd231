bdda22c0f755fb1d3f09103eeb499c7653cd38d2
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-02-04 18:12:44 -0700

    fix: enhance user info display on thank you page with improved formatting and additional membership details

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -17,11 +17,12 @@ function showInfo(word){
     userInfo.forEach((element) => {
         if(element.startsWith(word))
         {
-             value = element.split("=")[1].replace('%40', '@');
+             value = element.split("=")[1].replace('%40', '@').replaceAll("+", " ");
         }
         
     })
-    return(value);
+    //decodeURIComponent devuelve el string con : y . correctamente escritos
+    return(decodeURIComponent(value));
 }
 
 //Getting the user-info-container
@@ -30,12 +31,14 @@ const container = document.querySelector("#user-info-container");
 
 container.innerHTML = `
 <h2>Personal Info</h2>
-<p> Name: ${showInfo("fname")} ${showInfo("lname")}</p>
-<p>Phone Number : ${showInfo("telephone")}</p>
-<p>E-mail ${showInfo("email")}</p>
+<p> Name: <strong>${showInfo("fname")}</strong> <strong>${showInfo("lname")}</strong></p>
+<p>Phone Number : <strong>${showInfo("telephone")}</strong></p>
+<p>E-mail <strong>${showInfo("email")}</strong></p>
 <h2>Business Info</h2>
-<p>Business/Organization's Name: ${showInfo("Bname")}</p>
-<p>Applicant's title in the business: ${showInfo("title")}</p>
+<p>Business/Organization's Name: <strong>${showInfo("Bname")}</strong></p>
+<p>Applicant's title in the business: <strong>${showInfo("title")}</strong></p>
+<p>Type of Membership: <strong>${showInfo("membership-level")}</strong></p>
+<p>Business description: <strong>${showInfo("description")}</strong></p>
 
 
 
