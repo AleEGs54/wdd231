5c50b1d1c6a6d48fc3e761ebbe6d2dd90e9dd584
Alejandro Esteves <alejandro.esteves19@gmail.com>
2025-02-13 23:32:23 -0700

    feat: implement smooth scrolling and enhance membership card functionality with toggle feature

--- project/scripts/membership.js
+++ project/scripts/membership.js
@@ -1,3 +1,22 @@
 import styleNavAndFooter from "./common-scripts.js";
 
 styleNavAndFooter(); //To apply style to the nav and the footer
+
+//Opening the membership cards
+//Selecting the svgs
+const svgElements = document.querySelectorAll(".arrow");
+
+
+svgElements.forEach(ele => {
+
+    ele.addEventListener("click", () =>{
+
+        const cardContent = ele.closest('.card').querySelector('.card-content');
+        
+        // Toggle to show/hide
+        cardContent.classList.toggle("visible");
+
+
+    })
+
+})
